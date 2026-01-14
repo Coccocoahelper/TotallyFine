adbab9b3c2bdcdb3e6c761f5ae23737db3800974
thatonecoder (formerly Coccocoa's Helper) <157546848+Coccocoahelper@users.noreply.github.com>
2026-01-14 14:34:43 +0000

    Attempt to make it compile

--- src/main/java/io/thatonecoder/totallyfine/mixin/details/GameGuiMixin.java
+++ src/main/java/io/thatonecoder/totallyfine/mixin/details/GameGuiMixin.java
@@ -3,17 +3,18 @@ package io.thatonecoder.totallyfine.mixin.details;
 import io.thatonecoder.totallyfine.config.TotallyFineConfig;
 import net.minecraft.client.gui.GameGui;
 import net.minecraft.client.render.Window;
-import com.llamalad7.mixinextras.sugar.Local;
-import com.llamalad7.mixinextras.sugar.ref.LocalBooleanRef;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
 @Mixin(GameGui.class)
 public class GameGuiMixin {
 
 	@Inject(
-		method = "renderVignette"
+		method = "renderVignette",
+        at = @At("HEAD"),
+        cancellable = true
 	)
 	private void cancelVignette(float brightnessAtEyes, Window window, CallbackInfo ci) {
         if (TotallyFineConfig.instance.vignette.get()) {
