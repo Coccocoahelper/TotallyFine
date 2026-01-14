a0b0ce424f79e7825b4679376465a07fcd4fd568
thatonecoder (formerly Coccocoa's Helper) <157546848+Coccocoahelper@users.noreply.github.com>
2026-01-14 16:05:04 +0000

    gaslighting

--- src/main/java/io/thatonecoder/totallyfine/mixin/particleculling/ParticleManagerMixin.java
+++ src/main/java/io/thatonecoder/totallyfine/mixin/particleculling/ParticleManagerMixin.java
@@ -10,7 +10,7 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Redirect;
 
 @Mixin(ParticleManager.class)
-public abstract class ParticleManagerMixin {
+public class ParticleManagerMixin {
 
     @Redirect(
         method = "render",
